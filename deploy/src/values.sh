aa0ca812b68fd305d1737000e1908503031cfe25
atrejomora <atrejomora@vmware.com>
2021-08-23 13:32:23 -0600

    Adjustment to 'values' file and creation of YAMLs using included scripts.

--- deploy/src/values.sh
+++ deploy/src/values.sh
@@ -1,6 +1,6 @@
-export K8S_NAMESPACE=tacocat
-export K8S_APPLICATION=tacocat
-export K8S_CLUSTER=cluster1
+export K8S_NAMESPACE=polyglot-app
+export K8S_APPLICATION=polyglot-app
+export K8S_CLUSTER=tkg-cluster
 export K8S_LOCATION=americas
 export K8S_ENVIRONMENT=dev
-export K8S_REPOSITORY=192.168.1.8/demo-app
+export K8S_REPOSITORY=harbor.pdmodernapps.com/atrejomora
